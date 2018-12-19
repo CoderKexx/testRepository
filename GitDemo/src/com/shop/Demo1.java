4b633f73945165200aa0649d1fd0b532db48ef3e
kexx <kexx@qq.com>
2018-12-19 14:39:58 +0800

    把一个没用的属性去掉

--- GitDemo/src/com/shop/Demo1.java
+++ GitDemo/src/com/shop/Demo1.java
@@ -1,7 +1,6 @@
 package com.shop;
 
 public class Demo1 {
-	private String name;
 	public static void main(String[] args) {
 		System.out.println("第一次提交项目到git");
 		System.out.println("errro>>>>>>>>>>>>>");
