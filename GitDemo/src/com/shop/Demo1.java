1b94ea8ce6fae871a9dad0f735fb9a24c337e784
kegc <kegc@cndatacom.com>
2020-02-25 17:32:18 +0800

    del

--- GitDemo/src/com/shop/Demo1.java
+++ GitDemo/src/com/shop/Demo1.java
@@ -1,21 +1,6 @@
 package com.shop;
 
 public class Demo1 {
-	public static void main(String[] args) {
-		System.out.println("第一次提交项目到git");
-		System.out.println("errro>>>>>>>>>>>>>");
-		
-	}
-	public void lisi() {}
-
-	public void zs() {}
 	
-	public void day813() {
-		System.out.println("8月13日提交");
-	}
-	
-	public void day8131() {
-		System.out.println("8月13日提交");
-	}
 
 }
